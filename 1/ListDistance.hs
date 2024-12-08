0bd5119160a1f8bcd48cbf2dc5ecee442137cb68
Teemu Koski <80845642+TemeKoo@users.noreply.github.com>
2024-12-09 01:08:30 +0200

    Day 1 part 1 solution

--- 1/ListDistance.hs
+++ 1/ListDistance.hs
@@ -2,11 +2,22 @@ module ListDistance where
 
 import Help.Todo
 import System.FilePath
-import Control.Applicative
+import Data.List
 
 parseInput :: IO [Int]
 parseInput = readFile ("1" </> "input.txt") >>=
              \s  -> return . fmap read $ words s
 
-printInput :: IO ()
-printInput = parseInput >>= print . length
+sortInput :: [Int] -> [(Int, Int)]
+sortInput [] = []
+sortInput xs = zip (sort $ as xs) (sort $ bs xs)
+    where as [] = []
+          as (x:_:rs) = x:as rs
+          bs [] = []
+          bs (_:y:rs) = y:bs rs
+
+calculateDistances :: [(Int, Int)] -> Int
+calculateDistances = sum . map (\(a,b) -> abs (a-b))
+
+printResult :: IO ()
+printResult = parseInput >>= print . calculateDistances . sortInput
